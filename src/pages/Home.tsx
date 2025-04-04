5ee63ad64791e7ba3e283dacd510f8715604ba81
mairaj-byteifytech <mairaj@byteifytech.com>
2025-04-03 18:33:49 -0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Navbar from '../components/layout/Navbar';
+import Navbar from '../components/layout/Navbar-V';
 import Footer from '../components/layout/Footer';
 import Hero from '../components/sections/Hero';
 import Solutions from '../components/sections/Solutions';
